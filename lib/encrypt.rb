692fca9de889e40040b7f877a7a6dce123b8442b
TimothyFell <timothy.fell846@gmail.com>
2018-08-27 17:09:48 -0600

    final encrypt runner commit

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -3,11 +3,11 @@ require 'date'
 require 'pry'
 
 # Take in the args
-filename_1, filename_2 = ARGV
+orig_msg_filename, encrypted_filename = ARGV
 
 # open the files
-message_file = open("./lib/#{filename_1}", 'r')
-encrypted_file = open("./lib/#{filename_2}", 'w')
+message_file = open("./lib/#{orig_msg_filename}", 'r')
+encrypted_file = open("./lib/#{encrypted_filename}", 'w')
 
 # get and store the message to be encrypted
 message = message_file.read.strip
@@ -15,37 +15,10 @@ message = message_file.read.strip
 # create a new enigma
 e = Enigma.new
 
-# encrypt the message
-# if key == nil && date == nil
-
-  encrypted_message = e.encrypt(message)
-  encrypted_file.write(encrypted_message)
-  puts "Created '#{filename_2}' with the key #{e.key} and date #{Date.today.strftime("%d%m%y")}"
-
-# elsif key == nil
-#
-#   date_object = Date.strptime(date, "%d%m%y")
-#   encrypted_message = e.encrypt("#{message}", date_object)
-#   encrypted_file.write(encrypted_message)
-#   puts "#{encrypted_file.name}, #{e.key}, #{date_object}"
-#
-# elsif date == nil
-#
-#   encrypted_message = e.encrypt("#{message}", key)
-#   encrypted_file.write(encrypted_message)
-#   puts "#{encrypted_file.name}, #{key}, #{Date.today.strftime("%d%m%y")}"
-#
-# else
-#
-#   date_object = Date.strptime(date, "%d%m%y")
-#   encrypted_message = e.encrypt("#{message}", key, date_object)
-#   encrypted_file.write(encrypted_message)
-#   puts "#{encrypted_file.name}, #{key}, #{date_object}"
-#
-# end
-
-# write the encrypted message to the new file
-
+# encrypt and write the message
+encrypted_message = e.encrypt(message)
+encrypted_file.write(encrypted_message)
+puts "Created '#{encrypted_filename}' with the key #{e.key} and date #{Date.today.strftime("%d%m%y")}"
 
 # close both files
 encrypted_file.close
